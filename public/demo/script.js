a041dc74ae257ab98903c43a94faab11698d61c3
Miniontoby <tobias.gaarenstroom@gmail.com>
2022-11-17 17:33:04 +0100

    Added darkmode switcher and license text

--- public/demo/script.js
+++ public/demo/script.js
@@ -1,3 +1,21 @@
+/*
+    Plan2Go
+    Copyright (C) 2022  TheDutchProgrammers
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 let nav = 0;
 let clicked = null;
 let events = localStorage.getItem('events') ? JSON.parse(localStorage.getItem('events')) : [];
@@ -130,5 +148,15 @@ function initButtons() {
   document.getElementById('closeButton').addEventListener('click', closeModal);
 }
 
+function initDarkmode() {
+	const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]'), currentTheme = localStorage.getItem('theme');
+	if (currentTheme) {
+		document.documentElement.setAttribute('data-theme', currentTheme);
+		if (currentTheme === 'dark') toggleSwitch.checked = true;
+	}
+	toggleSwitch.addEventListener('change', function(e) { if (e.target.checked) { document.documentElement.setAttribute('data-theme', 'dark'); localStorage.setItem('theme', 'dark'); } else { document.documentElement.setAttribute('data-theme', 'light'); localStorage.setItem('theme', 'light'); } }, false);
+}
+
 initButtons();
+initDarkmode();
 load();
