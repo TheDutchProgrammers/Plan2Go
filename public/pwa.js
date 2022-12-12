285d8487aea5ada3ab418f72a96bf6bc87c60585
Miniontoby <tobias.gaarenstroom@gmail.com>
2022-12-12 11:21:45 -0700

    Added scheduled notifications

--- public/pwa.js
+++ public/pwa.js
@@ -1,5 +1,49 @@
 window.addEventListener("load", () => {
-  if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("service-worker.js");
-  }
+	if ("serviceWorker" in navigator) {
+		navigator.serviceWorker.register("service-worker.js");
+		navigator.serviceWorker.addEventListener('message', event => console.log(event.data));
+	}
 });
+
+document.querySelector('#notification-button').onclick = async () => {
+	const reg = await navigator.serviceWorker.getRegistration();
+	Notification.requestPermission().then(permission => {
+		if (permission !== 'granted') {
+			alert('you need to allow push notifications');
+		} else {
+			const timestamp = new Date().getTime() + 5 * 1000; // now plus 5000ms
+			reg.showNotification(
+				'Demo Push Notification',
+				{
+					tag: timestamp, // a unique ID
+					body: 'Hello World', // content of the push notification
+					showTrigger: new TimestampTrigger(timestamp), // set the time for the push notification
+					data: {
+						url: window.location.href, // pass the current url to the notification
+					},
+					badge: '/images/icon.png',
+					icon: '/images/icon.png',
+					actions: [
+						{
+							action: 'open',
+							title: 'Open app'
+						},
+						{
+							action: 'close',
+							title: 'Close notification'
+						}
+					]
+				}
+			);
+		}
+	});
+};
+document.querySelector('#notification-cancel').onclick = async () => {
+	const reg = await navigator.serviceWorker.getRegistration();
+	const notifications = await reg.getNotifications({
+		includeTriggered: true
+	});
+	notifications.forEach(notification => notification.close());
+	alert(`${notifications.length} notification(s) cancelled`);
+};
+
--- public/service-worker.js
+++ public/service-worker.js
@@ -89,3 +89,18 @@ self.addEventListener("fetch", (event) => {
   // will be handled by the browser as if there were no service
   // worker involvement.
 });
+
+self.addEventListener('notificationclick', event => {
+  if (event.action === 'close') {
+    event.notification.close();
+  } else {
+    event.waitUntil(self.clients.matchAll().then(clients => {
+      if(clients.length){ // check if at least one tab is already open
+        clients[0].focus();
+        clients[0].postMessage('Push notification clicked!');
+      } else {
+        self.clients.openWindow('/');
+      }
+    }));
+  }
+});
